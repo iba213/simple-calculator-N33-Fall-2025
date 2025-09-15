42c5b99c71762800c561dcd66c423ee2045f501c
elias <elias@DESKTOP-926F9T3>
2025-09-15 10:23:28 -0400

    rayan

---
+++ CalcLib/CalculatorEngine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace CalcLib
+{ 
+
+    public static class CalculatorEngine
+    {
+        public static double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            //You can use switch case also here for multiple operations or use if-else-if ladder
+            double result = 0;
+            
+
+            if (argOperation == "+" || argOperation.ToLower() == "add")
+            {
+                result = argFirstNumber + argSecondNumber;
+                
+            }
+            else if (argOperation == "*" || argOperation.ToLower() == "multiplication")
+            {
+                result = argFirstNumber * argSecondNumber;
+                
+            }
+            else if (argOperation == "/" || argOperation.ToLower() == "division")
+            {
+                result = argFirstNumber / argSecondNumber;
+               
+            }
+            else if (argOperation == "-" || argOperation.ToLower() == "substraction")
+            {
+                result = argFirstNumber - argSecondNumber;
+                
+            }
+            else if (argOperation.ToLower() == "modulus" || argOperation == "%")
+            {
+                result = argFirstNumber % argSecondNumber;
+                
+            }
+            else
+            {
+                throw new InvalidOperationException("");
+            }
+
+
+                return result;
+            }
+}
+    }
+
+ 
--- simple-calculator/Program.cs
+++ simple-calculator/Program.cs
@@ -1,37 +1,36 @@
-﻿using System;
+﻿using CalcLib;
+using SimpleCalculator;
+using System;
+using System.Text;
 
 
 namespace SimpleCalculator
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             try
             {
-                // Class to convert user input
-                InputConverter inputConverter = new InputConverter();
-
-                // Class to perform actual calculations
-                CalculatorEngine calculatorEngine = new CalculatorEngine();
+                
+                
                 Console.WriteLine("Welcome to Simple Calculator\nEnter  your first number then press Enter:");
-                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                double firstNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
                 Console.WriteLine("Enter  your Second number then press Enter:");
-                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-                Console.WriteLine("Enter operation like + or add number then press Enter:");
+                double secondNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.WriteLine("Enter operation wanted (add, +, etc) then press Enter:");
                 string operation = Console.ReadLine();
 
-                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+                double result = CalculatorEngine.Calculate(operation, firstNumber, secondNumber);
                 StringBuilder outputMessage = new StringBuilder();
                 Console.WriteLine(result);
 
             }
             catch (Exception ex)
             {
-                // Normally, we'd log this error to a file.
                 Console.WriteLine(ex.Message);
             }
-
+            
         }
     }
 }
